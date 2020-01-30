2c3e2d97f960be6c84bb0ce6ab482c402ef21377
kate-kate <kate@riverwaysoft.com>
2020-01-30 11:23:30 +0300

    box drawing

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,76 @@
 <template>
     <div id="app">
-        <h1>Object Labelling Demo</h1>
-        <div id="image-wrapper" :style="{backgroundImage: `url(caterpillar.jpg)`}">
-          <Box b-top="135" b-left="200" b-width="190" b-height="390"/>
+        <div id="image-wrapper" :style="{backgroundImage: `url(caterpillar.jpg)`}"
+             @mousedown="startDrawingBox" @mousemove="changeBox" @mouseup="stopDrawingBox">
+            <Box v-if="drawingBox.active" :b-width="drawingBox.width" :b-height="drawingBox.height"
+                 :b-top="drawingBox.top" :b-left="drawingBox.left"/>
+            <Box v-for="(box, i) in boxes" :key="i" :b-top="box.top" :b-left="box.left"
+                 :b-width="box.width" :b-height="box.height"/>
         </div>
     </div>
 </template>
 
 <script>
-
     import Box from "./components/Box";
+    import {pick} from 'lodash';
+
+    const getCoursorLeft = (e) => {
+        return e.pageX - 10;
+    };
+
+    const getCoursorTop = (e) => {
+        return e.pageY - 10;
+    };
+
     export default {
         name: 'app',
         components: {Box},
+        data: function () {
+            return {
+                drawingBox: {
+                    active: false,
+                    top: 0,
+                    left: 0,
+                    height: 0,
+                    width: 0
+                },
+                boxes: []
+            }
+        },
+        methods: {
+            startDrawingBox(e) {
+                this.drawingBox = {
+                    width: 0,
+                    height: 0,
+                    top: getCoursorTop(e),
+                    left: getCoursorLeft(e),
+                    active: true,
+                };
+            },
+            changeBox(e) {
+                if (this.drawingBox.active) {
+                    this.drawingBox = {
+                        ...this.drawingBox,
+                        width: getCoursorLeft(e) - this.drawingBox.left,
+                        height: getCoursorTop(e) - this.drawingBox.top,
+                    };
+                }
+            },
+            stopDrawingBox() {
+                if (this.drawingBox.active) {
+                    if (this.drawingBox.width > 5) {
+                        this.boxes.push({...pick(this.drawingBox, ['width', 'height', 'top', 'left'])});
+                    }
+                    this.drawingBox = {
+                        active: false,
+                        top: 0,
+                        left: 0,
+                        height: 0,
+                        width: 0
+                    }
+                }
+            },
+        }
     }
 </script>
 
@@ -23,7 +81,6 @@
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
-        margin-top: 60px;
 
         #image-wrapper {
             height: 640px;
